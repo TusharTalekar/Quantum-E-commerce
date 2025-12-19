27cbfb392390d071bf1c262b03b2c3f7a5e4ef5a
TusharTalekar <tushartalekar2024@gmail.com>
2025-12-19 16:32:53 +0530

    changing VITE_BACKEND_URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,1 +1,1 @@
-export const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+export const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://quantum-two-delta.vercel.app";
